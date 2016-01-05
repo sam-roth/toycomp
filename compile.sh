96478468eb35aecd77d55fd57a0a705c82ecbb63
Sam Roth <sam.roth1@gmail.com>
2016-01-04 20:55:30 -0800

    Remove unnecessary compile.sh script.

--- compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TARGET_TRIPLE="$(llvm-config --host-target)"
-
-python -m toycomp.driver "$1" --triple "$TARGET_TRIPLE" > a.ll
-llc a.ll -o a.s -mtriple "$TARGET_TRIPLE"
-clang a.ll lib.c libmain.c
